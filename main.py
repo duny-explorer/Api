9314274331e6ffbefea1539448487aaa5683424d
duny-explorer <37844052+duny-explorer@users.noreply.github.com>
2019-04-07 22:58:43 +0300

    Add files via upload
    ААААААААААААААААААААА

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ class Example(QMainWindow):
     
     def keyPressEvent(self, e):
         try:
-            x_step = 359 / 2 ** self.zooming * 2
-            y_step = 180 / 2 ** self.zooming * 2
+            self.x_step = 360 / 2 ** self.zooming * 1.76
+            self.y_step = 180 / 2 ** self.zooming * 2
+            
             if e.key() == Qt.Key_PageUp:
                 if self.zooming < 19:
                     self.zooming += 1
@@ -28,30 +29,30 @@ class Example(QMainWindow):
                     self.show_map_file()
                     
             elif e.key() == Qt.Key_Down:
-                coord = float(self.lat_input.text()) - y_step
+                coord = float(self.lat_input.text()) - self.y_step
                 if coord < -85:
                     coord = -85
                 self.lat_input.setText(str(coord))
                 self.show_map_file()
                 
             elif e.key() == Qt.Key_Up:
-                coord = float(self.lat_input.text()) + y_step
+                coord = float(self.lat_input.text()) + self.y_step
                 if coord > 85:
                     coord = 85
                 self.lat_input.setText(str(coord))
                 self.show_map_file()    
                 
             elif e.key() == Qt.Key_Right:
-                coord = float(self.lon_input.text()) + x_step
+                coord = float(self.lon_input.text()) + self.x_step
                 if coord > 180:
-                    coord = -(180 - x_step)
+                    coord = -(180 - self.x_step)
                 self.lon_input.setText(str(coord))
                 self.show_map_file()   
                 
             elif e.key() == Qt.Key_Left:
-                coord = float(self.lon_input.text()) - x_step
+                coord = float(self.lon_input.text()) - self.x_step
                 if coord < -180:
-                    coord = 180 - x_step                
+                    coord = 180 - self.x_step                
                 self.lon_input.setText(str(coord))
                 self.show_map_file()            
             
@@ -68,6 +69,8 @@ class Example(QMainWindow):
         self.pixmap = QPixmap.fromImage(ImageQt(self.image))
         self.label.setPixmap(self.pixmap)
 
+        self.label.mousePressEvent = self.get_pos
+
         self.type_layout.addItems(["Схема", "Спутник", "Гибрид"])
         self.type_layout.activated[str].connect(self.change_type)
 
@@ -79,18 +82,51 @@ class Example(QMainWindow):
         self.layout = "map"
         self.zooming = 8
         self.mark = False
+        self.x_step = (360 / 2 ** self.zooming) * 2
+        self.y_step = (180 / 2 ** self.zooming) * 2
 
+
+    def get_pos(self, event):
+        button = event.button()
+        self.x_step = (360 / 2 ** self.zooming) * 1.76
+        self.y_step = (180 / 2 ** self.zooming) * 2            
+        x, y = event.pos().x() - 225, event.pos().y() - 225
+        x = float(self.lon_input.text()) + self.x_step / 450 * x
+        y = float(self.lat_input.text()) - self.y_step / 450 * y
+
+        if button == Qt.LeftButton:
+            self.clear_mark()
+            self.mark = [x, y]
+            self.obj = geocode('{},{}'.format(x, y))
+            self.address.setPlainText(self.exist_check())            
+            self.show_map_file()
+        elif button == Qt.RightButton:
+            self.clear_mark() 
+            self.mark = [x, y] 
+            self.obj = find_org('{},{}'.format(x, y), '0.0001,0.0001', None) 
+            self.address.setPlainText(self.exist_check()) 
+            self.show_map_file()
+            
 
     def change_index(self, state):
-        if self.address.text():
-            self.address.setText(self.exist_check())
+        if self.address.toPlainText():
+            self.address.setPlainText(self.exist_check())
 
 
     def exist_check(self):
-        return "{}. Индекс: {}".format(self.obj['properties']['CompanyMetaData']["address"], self.obj['properties']['CompanyMetaData']["postalCode"]) \
-    if self.index.isChecked() == True else self.obj['properties']['CompanyMetaData']["address"]
-            
 
+        address = self.obj["metaDataProperty"]["GeocoderMetaData"]["text"] if "metaDataProperty" in self.obj else \
+                  self.obj['properties']['CompanyMetaData']["address"]
+
+        if "metaDataProperty" in self.obj and "postal_code" in self.obj["metaDataProperty"]["GeocoderMetaData"]["Address"]:
+            index = self.obj["metaDataProperty"]["GeocoderMetaData"]["Address"]["postal_code"]
+        elif "properties" in self.obj and "postalCode" in self.obj['properties']['CompanyMetaData']:
+            index = self.obj['properties']['CompanyMetaData']["postalCode"]
+        else:
+            index = ""
+
+        return "{}. Индекс: {}".format(address, index)
+ 
 
     def change_type(self, type_map):
         self.layout = {"Схема": "map", "Спутник": "sat", "Гибрид": "skl"}[type_map]
@@ -99,7 +135,7 @@ class Example(QMainWindow):
 
     def clear_mark(self):
         self.mark = False
-        self.address.setText("")
+        self.address.setPlainText("")
         self.show_map_file()
         
 
@@ -107,13 +143,11 @@ class Example(QMainWindow):
         try:
             if self.sender() is not None and type(self.sender()) != type(self.type_layout) and self.sender().text() == "Искать":
                 self.zooming = 19
-                self.obj = find_org(get_coord(self.search.text()), "0.005,0.005", self.search.text())
-                self.address.setText(self.exist_check())
-                self.mark = self.obj['geometry']['coordinates']
+                self.obj = geocode(self.search.text())
+                self.address.setPlainText(self.exist_check())
+                self.mark = self.obj["Point"]["pos"].split() if "Point" in self.obj else self.obj['geometry']['coordinates']
                 self.lat_input.setText(str(self.mark[1]))
                 self.lon_input.setText(str(self.mark[0]))
-
-          
                 self.search.setText("")
       
             params = {"ll": ",".join([self.lon_input.text(),self.lat_input.text()]),
@@ -122,7 +156,7 @@ class Example(QMainWindow):
                       "size": "450,450"}
 
             if self.mark:
-                params["pt"] = "{},{}".format(*self.mark) + ",pm2bl"
+                params["pt"] = "{},{}".format(*self.mark) + ",pm2bll"
                 
             f_name = get_file_map(params)
         
@@ -143,4 +177,5 @@ if __name__ == '__main__':
     ex = Example()
     ex.show()
     sys.exit(app.exec())
+
 
